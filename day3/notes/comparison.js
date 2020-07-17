b57ba2051cdd2f54477c4a11d514bea8833f86a5
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-07-17 12:17:35 +0530

    Update day 2 assignment and day 3 notes

---
+++ day3/notes/comparison.js
@@ -0,0 +1,33 @@
+console.log("Comparison and logical operators");
+
+/*
+Comparison
+1. >
+2. <
+3. >=
+4. <=
+5. == equals (value)
+6. !=
+7. === strict equality (type)
+8. !== not strict 
+
+
+Logical
+1. and - &&
+2. or ||
+*/
+console.log(null <= 0);
+// why is that true when null >, <, or == 0 is false?
+// the reason is that >= or <= converts into Number
+// null <= 0 becomes Number(null) <= 0 wich is 0 <=0 which is true
+
+console.log(null);
+console.log(Number(null));  // result is 0
+
+console.log(undefined);
+console.log(Number(undefined));  // should output NaN
+
+// lexicographical order
+console.log("abce">"abd");  // false
+console.log("abce">"abcd");  // true
+
